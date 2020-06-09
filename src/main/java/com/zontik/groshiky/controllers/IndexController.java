07615445dd9467b1b4bb544526ec34642c104623
017zontik <017zontik@gmail.com>
2020-06-09 11:34:28 +0300

    fix for registration page

--- src/main/java/com/zontik/groshiky/controllers/IndexController.java
+++ src/main/java/com/zontik/groshiky/controllers/IndexController.java
@@ -1,9 +1,12 @@
 package com.zontik.groshiky.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
@@ -15,4 +18,8 @@ public class IndexController {
         return "index";
     }
 
+    @RequestMapping(value = "/registration", method = RequestMethod.GET)
+    public String registration(ModelMap model) {
+        return "registration";
+    }
 }
