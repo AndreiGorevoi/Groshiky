9248da2239b87d083493fbc7c756239e0fcb09b1
017zontik <017zontik@gmail.com>
2020-06-10 17:09:03 +0300

    UserService class with interface were added

---
+++ src/main/java/com/zontik/groshiky/service/IUserService.java
@@ -0,0 +1,7 @@
+package com.zontik.groshiky.service;
+
+import com.zontik.groshiky.model.User;
+
+public interface IUserService {
+    void createUser (User user);
+}
---
+++ src/main/java/com/zontik/groshiky/service/UserService.java
@@ -0,0 +1,20 @@
+package com.zontik.groshiky.service;
+
+import com.zontik.groshiky.dao.IUserDao;
+import com.zontik.groshiky.dao.UserDao;
+import com.zontik.groshiky.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+public class UserService implements IUserService{
+    @Autowired
+    private IUserDao userDao;
+
+    @Override
+    public void createUser(User user) {
+        userDao.createUser(user);
+    }
+}
